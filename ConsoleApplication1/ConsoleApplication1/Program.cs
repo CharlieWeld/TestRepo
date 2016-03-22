000c23823d413c035412675dc35dbb584150a2a2
unknown <charles.weld@mydit.ie>
2016-03-22 18:38:11 +0000

    greg

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
 
-		Console.WriteLine("Edited by Greg & Tom");
+		Console.WriteLine("Edited by Greg");
 
         }
     }
